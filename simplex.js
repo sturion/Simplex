f2e2eb59ef00f1c9cd9ccdfdf75c4b4bd781bfb9
LeonardoVinagre <lvinagre2.0@gmail.com>
2022-10-07 14:46:56 -0300

    Metodo simplex funcional

--- simplex.js
+++ simplex.js
@@ -1,10 +1,9 @@
 import {table, checkLen} from './table.js';
 import {verifyAll} from './verifyFields.js';
 
-var mock = [[0,3,2,1,0,0,180],
-            [0,2,4,0,1,0,240],
-            [0,6,5,0,0,1,600],
-            [1,-21,-20,0,0,0,0]
+var mock = [[0,2,1,1,0,6],
+            [0,10,12,0,1,60],
+            [1,-5,-2,0,0,0]
          ];
 
 
@@ -35,7 +34,7 @@ const findPivoRow = (table, pivoColumn) => {
    var pivoRow;
    
    for(let i = 0 ; i < tableLen-1 ; i++){
-      if(table[i][tableVetLen-1]/table[i][pivoColumn] < minorPositive){
+      if(table[i][tableVetLen-1]/table[i][pivoColumn] < minorPositive && table[i][tableVetLen-1]/table[i][pivoColumn] > 0){
          minorPositive = table[i][tableVetLen-1]/table[i][pivoColumn];
          pivoRow = i;
       }
@@ -53,71 +52,81 @@ const newRowPivo = (table, pivoColumn, pivoRow) => {
    return nrP;
 }
 
-const newRows = (table, nrP, pivoColumn) => {
+const newRows = (table, pivoColumn, pivoRow, nrP) => {
    var newRows = new Array();
    let tableLen = checkLen(table);
    let tableVetLen = checkLen(table[0]);
-   let nlCoef = new Array();
-   console.log(table);
-   console.log(nrP);
-   for(let i = 0 ; i < tableLen-1 ; i++){
-      newRows[i] = new Array();
-      nlCoef[i] = new Array();
+
+   for(let i = 0 ; i < tableLen ; i++){
+      if(i == pivoRow) continue;
+
+      let nlCoef = new Array();
+      
+      //pega o coef(*-1)
       let coef = (table[i][pivoColumn])*-1;
 
-      //table => pegar o coef(*-1) e multiplicar pela nrP
-      
+      // multiplicar o coef pela nrP
       nrP.forEach(element => {
-         nlCoef[i].push(coef*element);
+         nlCoef.push(coef*element);
       });
-      
 
-      // table[i].forEach(element => {
-      //    newRows[i].push(element+nlCoef[i]);
-      // });
-
+      //soma a row antiga com a do coeficiente 
+      let nRowAux = new Array();
       for(let j = 0 ; j < tableVetLen ; j++){
          
-         newRows[i].push(table[i][j]+nlCoef[i][j])
+         nRowAux.push(table[i][j]+nlCoef[j])
       }
+      newRows.push(nRowAux);  
    }
-   
-   console.log(nlCoef);
-   console.log(newRows);
+
    return newRows;
 }
+
+const regroupRows = (nrP, nlX, pivoRow) => {
+   var newTable = nlX;
+   newTable.splice(pivoRow, 0, nrP);
+   return newTable;
+}
+
 
 function simplexMain(){
    
    if(verifyAll){
       const myTable = table()[0];
       const vetLen = table()[1];
-      runMethod(mock);
+      runMethod(mock,0);
    }
 }
 
 
-const runMethod = (myTable) => {
+const runMethod = (myTable,aux) => {
+   var aux = aux;
    var table = myTable;
    //mostrar solução (vb,vnb,z)
    //1-verifica se tem negatividade na max(z)
    if(!checkZ(table) == true){
       console.log('n tem negativo');
+      console.log(table);
       return 0;//resposta final
    }
    //2-loop para achar coluna pivo(maior negativo)
    const pivoColumn = findPivoColumn(myTable);
    //3-achar linha pivo, dividingo o resultado pela coluna pivo de cada um, o menr resultado positivo segue
    const pivoRow = findPivoRow(myTable, pivoColumn);
+   
    //4-calcular nova linha pivo
    const nrP = newRowPivo(myTable, pivoColumn, pivoRow);
    //5-calcular linhas restantes
-   const nlX = newRows(myTable, nrP, pivoColumn);
+   const nlX = newRows(myTable, pivoColumn, pivoRow, nrP);
    //6-juntar linhas em uma tabela
-   //recurssiva
-   // console.log(myTable);
-   // console.log(pivoColumn);
-   // console.log(pivoRow);
+   const newTable = regroupRows(nrP, nlX, pivoRow);
+   
+   console.log(newTable);
+   if(aux < 4){
+      aux++;
+      runMethod(newTable,aux)
+   }
+   //runMethod(newTable);
 }
 simplexMain();
 export {simplexMain};
